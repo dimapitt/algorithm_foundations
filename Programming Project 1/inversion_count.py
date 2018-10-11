1582b7fc0b2610b06503d15ff2c1f1c11bf4de35
Dmitry Kalika <dimapitt@gmail.com>
2018-10-11 18:53:01 -0400

    Added inversion ratio

--- Programming Project 1/inversion_count.py
+++ Programming Project 1/inversion_count.py
@@ -1,8 +1,14 @@
 
-def count(array):  # Dumb way to count inversions
+def count(array, ratio_out=False):  # Dumb way to count inversions
     inv_count = 0
+    max_inv = 0
     for i in range(len(array)):
+        max_inv += i
         for j in range(i+1, len(array)):
+
             if array[i] > array[j]:
                 inv_count += 1
-    return inv_count
+    if ratio_out:
+        return inv_count/max_inv  # return inversion ratio
+    else:
+        return inv_count  # return # of inversion
